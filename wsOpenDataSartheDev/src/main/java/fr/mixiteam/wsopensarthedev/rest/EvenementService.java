e42bc2497df5c78db6d88a02a615168e106a66f2
user <user@pc>
2016-01-23 16:48:40 +0100

    hgf

--- wsOpenDataSartheDev/src/main/java/fr/mixiteam/wsopensarthedev/rest/EvenementService.java
+++ wsOpenDataSartheDev/src/main/java/fr/mixiteam/wsopensarthedev/rest/EvenementService.java
@@ -15,7 +15,7 @@ import javax.ws.rs.core.MediaType;
 import fr.mixiteam.wsopensarthedev.modele.Evenement;
 
 
-@Path("/Evenementservice/")
+@Path("/Evenement/")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class EvenementService {
@@ -23,16 +23,7 @@ public class EvenementService {
 
 	public void init() {
 
-		Evenement newEvenement1 = new Evenement();
-		newEvenement1.setId(1);
-		newEvenement1.setName("Alvin Reyes");
-
-		Evenement newEvenement2 = new Evenement();
-		newEvenement2.setId(2);
-		newEvenement2.setName("Rachelle Ann de Guzman Reyes");
-
-		evenements.put("1", newEvenement1);
-		evenements.put("2", newEvenement2);
+		
 
 	}
 
@@ -41,14 +32,14 @@ public class EvenementService {
 	}
 
 	@POST
-	@Path("/Evenements/{id}/")
+	@Path("/{id}/")
 	public Evenement getEvenement(@PathParam("id") String id) {
 		Evenement c = evenements.get(id);
 		return c;
 	}
 
 	@POST
-	@Path("/Evenements/getall")
+	@Path("/getall")
 	public List getAllEvenements(Evenement Evenement) {
 		List EvenementList = new ArrayList();
 		for (int i = 0; i <= evenements.size(); i++) {
@@ -58,7 +49,7 @@ public class EvenementService {
 	}
 	
 	@GET
-	@Path("/Evenements/all")
+	@Path("/all")
 	public List<Evenement> getAllEvenements() {
 		List<Evenement> EvenementList = new ArrayList<Evenement>();
 		for (int i = 0; i <= evenements.size(); i++) {
